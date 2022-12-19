a53435d969d7aaf65966ea1a879892aff21b910c
Falko Löffler <falko.loeffler@arivis.com>
2022-12-19 19:47:57 +0100

    add curve parameters

--- OSPRaySharp/Geometries/OSPCurveGeometry.cs
+++ OSPRaySharp/Geometries/OSPCurveGeometry.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,20 @@ namespace OSPRay.Geometries
         {
         }
 
-        // TODO: add properties
+        public void SetPositionRadius(Vector4[] positionRadius) => SetArrayParam("vertex.position_radius", positionRadius);
+        public void SetTexcoord(Vector2[] texcoord) => SetArrayParam("vertex.texcoord", texcoord);
+        public void SetColor(Vector4[] color) => SetArrayParam("vertex.color", color);
+        public void SetNormal(Vector3[] normal) => SetArrayParam("vertex.normal", normal);
+        public void SetTangent(Vector3[] tangent) => SetArrayParam("vertex.tangent", tangent);
+        public void SetIndex(int[] index)
+        {
+            using (var data = OSPDataFactory.CreateArray(index, OSPDataType.UInt, index.Length))
+            {
+                SetObjectParam("index", data);
+            }
+        }
+        public void SetType(OSPCurveType type) => SetParam("type", type);
+        public void SetBasis(OSPCurveBasis basis) => SetParam("basis", basis);
+
     }
 }
---
+++ OSPRaySharp/Geometries/OSPCurveType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSPRay.Geometries
+{
+    public enum OSPCurveType
+    {
+        Round,
+        Flat,
+        Ribbon,
+        Disjoint,
+    }
+}
