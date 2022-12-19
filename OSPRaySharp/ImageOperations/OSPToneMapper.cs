0ab7a0c376b294abfac85aa51b56c8616a0be91c
Falko Löffler <falko.loeffler@arivis.com>
2022-12-19 15:49:54 +0100

    add tonemapper params

--- OSPRaySharp/ImageOperations/OSPToneMapper.cs
+++ OSPRaySharp/ImageOperations/OSPToneMapper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,14 @@ namespace OSPRay.ImageOperations
         public OSPToneMapper(): base("tonemapper")
         {
         }
+
+        public void SetExposure(float exposure) => SetParam("exposure", exposure);
+        public void SetContrast(float contrast) => SetParam("contrast", contrast);
+        public void SetShoulder(float shoulder) => SetParam("shoulder", shoulder);
+        public void SetMidIn(float midIn) => SetParam("midIn", midIn);
+        public void SetMidOut(float midOut) => SetParam("midOut", midOut);
+        public void SetHdrMax(float hdrMax) => SetParam("hdrMax", hdrMax);
+        public void SetAcesColor(bool acesColor) => SetParam("acesColor", acesColor);
+
     }
 }
